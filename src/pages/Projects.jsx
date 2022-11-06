9bd2831ffcc7c76afd09655eae8465999b4b1b8f
Kingtero28 <danielkq8@gmail.com>
2022-11-06 07:04:13 -0500

    change

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -6,7 +6,7 @@ const Projects = () => {
     <body className='projects'>
       <div className='container'>
         <div className='serviceBox'>
-          <div className='icon'><video src="/public/videos/WeatherApp.mp4" autoPlay muted loop></video></div>
+          <div className='icon'><video src="/videos/WeatherApp.mp4" autoPlay muted loop></video></div>
           <div className='content'>
             <h2>Weather</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -14,7 +14,7 @@ const Projects = () => {
           </div>
         </div>
         <div className='serviceBox'>
-          <div className='icon'><video src="/public/videos/CrudsApp.mp4" autoPlay muted loop></video></div>
+          <div className='icon'><video src="/videos/CrudsApp.mp4" autoPlay muted loop></video></div>
           <div className='content'>
             <h2>CRUD's</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -22,7 +22,7 @@ const Projects = () => {
           </div>
         </div>
         <div className='serviceBox'>
-          <div className='icon'><video src="/public/videos/QuoteBoxApp.mp4" autoPlay muted loop></video></div>
+          <div className='icon'><video src="/videos/QuoteBoxApp.mp4" autoPlay muted loop></video></div>
           <div className='content'>
             <h2>QuoteBox</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
@@ -30,7 +30,7 @@ const Projects = () => {
           </div>
         </div>
         <div className='serviceBox'>
-          <div className='icon'><video src="/public/videos/RickAndMortyApp.mp4" autoPlay muted loop></video></div>
+          <div className='icon'><video src="/videos/RickAndMortyApp.mp4" autoPlay muted loop></video></div>
           <div className='content'>
             <h2>Rick And Morty</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
